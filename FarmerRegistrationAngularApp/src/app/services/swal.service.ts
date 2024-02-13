abef2515a67986e339a0bad9061096f867f8c7f7
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2024-02-13 12:15:01 +0530

    Swal Service

--- FarmerRegistrationAngularApp/src/app/services/swal.service.ts
+++ FarmerRegistrationAngularApp/src/app/services/swal.service.ts
@@ -17,6 +17,26 @@ export class SWALService {
     })
   }
 
+  successSWALNoButton(title: string, text: string) {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: title,
+      text: text,
+      showConfirmButton: false,
+      timer: 2000
+    });
+  }
+
+  successConfirmSWAL(title: string, text: string) {
+    return Swal.fire({
+      title: title,
+      text: text,
+      icon: 'success',
+      confirmButtonText: 'Yes',
+    })
+  }
+
   errorSWAL(title: string, text: string) {
     Swal.fire({
       title: title,
@@ -44,6 +64,7 @@ export class SWALService {
     })
   }
 
+
   questionSWAL(title: string, text: string) {
     Swal.fire({
       title: title,
@@ -64,15 +85,72 @@ export class SWALService {
     })
   }
 
+  confirmInfoSWAL(title: string, text: string) {
+    return Swal.fire({
+      title: title,
+      text: text,
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonText: 'Login'
+    })
+  }
+
   questionConfirmSWAL(title: string, text: string) {
     return Swal.fire({
       title: title,
       text: text,
       icon: 'question',
       showCancelButton: true,
-      confirmButtonText: '<i class="fa fa-check"></i>' +' Yes',
+      confirmButtonText: 'Yes, Confirm!',
       cancelButtonText: 'No',
       confirmButtonColor: title.toUpperCase() == 'DELETE' ? '#ce0101' : '#6631b6',
+    })
+  }
+
+  wardOccupyingAlert(res: any, selectedWardDetails: any) {
+    const wardName = selectedWardDetails.wardName;
+    const bedGiven = res.data.bedGiven;
+    const bedOccupied = Number(res.data.bedOccupied) + 1;
+
+    Swal.fire({
+      title: 'Ward Status',
+      html: `The total bed strength of the ward:
+    <b style="color: #044c80">${wardName}</b> is
+    <b style="color: #e50101">${bedGiven}</b>,
+    but you are trying to occupy :
+    <b style="color: #e50101">${bedOccupied}</b>`,
+      icon: 'info'
+    });
+  }
+
+  confirmWardOccupyingAlert(res: any, selectedWardDetails: any) {
+    const wardName = selectedWardDetails.wardName;
+    const bedGiven = res.data.bedGiven;
+    const bedOccupied = Number(res.data.bedOccupied) + 1;
+
+    return Swal.fire({
+      title: 'Ward Status',
+      html: `The total bed strength of the ward:
+    <b style="color: #044c80">${wardName}</b> is
+    <b style="color: #e50101">${bedGiven}</b>,
+    but you are trying to occupy :
+    <b style="color: #e50101">${bedOccupied}</b>`,
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Confirm!',
+      cancelButtonText: 'No',
+      confirmButtonColor: '#6631b6',
+    });
+  }
+
+  confirmSuccessManualSWAL(title: string, text: string, confirm: string, cancel: string) {
+    return Swal.fire({
+      title: title,
+      text: text,
+      icon: 'success',
+      showCancelButton: true,
+      confirmButtonText: confirm,
+      cancelButtonText: cancel
     })
   }
 }
